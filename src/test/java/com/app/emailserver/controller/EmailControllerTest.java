cacffe81471b71c29700e51bf9fc1b4e2e671030
Andres Benavides <dandavi2310@hotmail.com>
2024-06-21 04:36:26 -0500

    update pipeline to add sonarcloud and unit test

---
+++ src/test/java/com/app/emailserver/controller/EmailControllerTest.java
@@ -0,0 +1,4 @@
+package com.app.emailserver.controller;
+
+public class EmailControllerTest {
+}
---
+++ src/test/java/com/app/emailserver/usecase/EmailControllerUseCaseTest.java
@@ -0,0 +1,4 @@
+package com.app.emailserver.usecase;
+
+public class EmailControllerUseCaseTest {
+}
